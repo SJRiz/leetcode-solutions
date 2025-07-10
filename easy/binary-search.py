bd70946816ee3edd838cdf5e1bc076d0de23be03
SJRiz <rizvij006@gmail.com>
2025-07-09 21:06:56 -0400

    Solved binary search

---
+++ easy/binary-search.py
@@ -0,0 +1,29 @@
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+# If target exists, then return its index. Otherwise, return -1.
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+
+            # Half the index each time
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            
+            # If we overshoot, then we know everything to the right of the current middle (including the current middle) is also an overshoot
+            elif nums[m] < target:
+                l = m + 1
+                
+            # Likewise if we undershoot
+            else:
+                r = m - 1
+        
+        return -1
