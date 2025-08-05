6cb243d5d8f7d6e0d1e1cb8078075bd8a3fd18f0
SJRiz <rizvij006@gmail.com>
2025-08-05 18:39:19 -0400

    solved longest palindromc substring

---
+++ medium/longest-palindromic-substring.py
@@ -0,0 +1,32 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+"""
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        # expand outwards, check both odd and even
+        res = (0, 0)
+
+        for i in range(len(s)):
+
+            # odd:
+            l, r = i, i
+            while (l >= 0 and r < len(s)) and s[l] == s[r]:
+                if (r-l+1) > (res[1] - res[0] + 1):
+                    res = (l, r)
+                l -= 1
+                r += 1
+
+
+            # even:
+            l, r = i, i+1
+            while (l >= 0 and r < len(s)) and s[l] == s[r]:
+                if (r-l+1) > (res[1] - res[0] + 1):
+                    res = (l, r)
+                l -= 1
+                r += 1
+
+        return s[res[0]:res[1]+1]
+    
+    # O(n^2) time and O(1) space
